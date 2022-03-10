be6da61282288a1c0dd1110839933467f7493df8
Cyrus <cyrus.cl@outlook.com>
2022-03-10 17:24:11 -0500

    first iteration run.

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,5 +1,7 @@
 import logging
-
+import argparse
+import subprocess, shlex
+import re
 
 def getLogger(name, level):
     logger = logging.getLogger(name)
@@ -24,7 +26,12 @@ def getLogLevel(level):
     else:
         return logging.DEBUG
 
- 
+
+def runCmd(cmd):
+    subprocess.run(shlex.split(cmd), capture_output=True, check=True, text=True)
+
+    # subprocess.run(shlex.split(cmd), check=True, text=True, shell=True)
+    
 def gettcs(prog, iter):
     """run .
     Args:
@@ -35,22 +42,19 @@ def gettcs(prog, iter):
     file_name = progbase.split(".")
     progname = file_name[0]
     compile_cmd=f"gcc {prog} -o {progpath}/{progname}"
-    subprocess.run(shlex.split(compile_cmd), capture_output=True, check=True, text=True)
-
+    runCmd(compiled_cmd)
     trace_path = f"{progpath}/traces"
     if not os.path.exists(trace_path):
         os.makedirs(trace_path)
     else :
         shutil.rmtree(trace_path)
         os.makedirs(trace_path)
-    # tracehash = {}
+
     trace_file = f"{trace_path}/{progname}.tcs"
     fw = open(trace_file, "w")
     # print("vtrace28; I x; I y; I c", file=fw)
-    # print("vtrace25; I x; I y; I c", file=fw)
-
+ 
     for i in range(iter):
-        # trace_file = f"{trace_path}/{progname}_{i}.tcs"
         nondet_input = randint(-50, 50)
         # with open(trace_file, 'w+') as f:
         subprocess.call(['./' + progpath +'/'+ progname, str(nondet_input), "0"], stdout=fw)
@@ -58,27 +62,37 @@ def gettcs(prog, iter):
     fw.close()
     print("trace generated!")
 
+ 
 
-
-def init_prog (program):
-    pass
-
-
-def run_dig (traces):
-    pass
-
-
-def parse_results(file_invs):
+def processInvars(file_invs, file_processed):
     fr = open(file_invs, "r")
+    # x = fileInvs.split(".")
+    # outInvs = x[0]+"_refine.inv"
+    # print(outInvs)
     invsList = []
     for line in fr:
         itermList = re.split(";", line.rstrip('\n'))
-        invsList = invsList + itermList
-    return invsList
+        invsList.append(itermList)
+    fw = open(file_processed, 'w')
+    for traceVars in invsList:
+        invars = []
+        # invars.append(traceVars[0])
+        for inv in traceVars[1:]:
+            # print(inv[-1])
+            if inv[-1] == "1":
+                invars.append(inv[:-1])
+        # print(';'.join(invars))
+        fw.writelines(traceVars[0]+';'+'&&'.join(invars)+'\n')
+    fw.close            
+    print (invsList)
 
-def run_instr(program, invars):
+def processTrace(fileTrace):
     pass
-    
-def run_ultimate (program):
-    pass
-    
+  
+if __name__ == "__main__":
+    aparser = argparse.ArgumentParser("Run c program to collect traces.")
+    ag = aparser.add_argument
+    ag("--inp", "-i", type=str, help="input c program")
+    args = aparser.parse_args()
+    # main(args.inp, args.n, args.p, args.v)
+    processInvars(args.inp)
