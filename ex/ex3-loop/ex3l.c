628c12fee3c057d3f0ed432e4ccc6c13a33db5d5
Ton Chanh Le <letonchanh@gmail.com>
2022-04-07 02:41:25 +0000

    ex3 with rand inps

--- ex/ex3-loop/ex3l.c
+++ ex/ex3-loop/ex3l.c
@@ -1,3 +1,5 @@
+int term = 0;
+
 int main() {
     int x, y;
     // while( (x<0 && y>0) || (y<0 && x>0) ) {
@@ -5,5 +7,6 @@ int main() {
       x = x + 1;
       y = y + 1;
     }
+    term = 1;
     return 0;
 }
