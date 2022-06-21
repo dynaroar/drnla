4ea5acc5116e627dcbe651e65cc319f1a4cb182a
Chanh Le <letonchanh@gmail.com>
2022-06-21 09:27:24 -0400

    C versions of T2 examples

---
+++ benchmarks/ctl-nla1/ax2-nla-valid_fn.c
@@ -0,0 +1,20 @@
+// ARGS: -precondition "c == 0 && c < k" -domain polyhedra
+
+int main() {
+    int k, x, y, c;
+
+    x = 0;
+    y = 0;
+
+    int p;
+    p = 1;
+
+    while (c + (y * y) - 2 * x + y < k) {
+        p = 0;
+        c = c + 1;
+        y = y + 1;
+        x = y + x;
+    }
+
+    p = 0;
+}
