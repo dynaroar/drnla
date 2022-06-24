177c6e72b0a36c8f1a1eec0da0937a7ca167a2db
Ton Chanh Le <letonchanh@gmail.com>
2022-06-24 13:23:00 +0000

    FN version

---
+++ benchmarks/ctl-pldi13small-nla-rest/neg-efafp-nla-invalid_fn.c
@@ -0,0 +1,40 @@
+//CTL [AG]([EG](p<=0))  
+// ARGS: -domain polyhedra
+
+int main() {
+    int a, n, x, y, z;
+    a = ?;
+    n = 0;
+    x = 0;
+    y = 1;
+    z = 6;
+    int c = 0, k = ?;
+
+    int p, xp, rho1;
+    p = 0;
+    xp = 5;
+
+
+    while(xp>=0){
+        rho1 = ?;
+
+        if (rho1>0){
+            xp = xp-1;
+        } else {
+            p = 0;
+         }
+
+    }
+ 
+    while ((z*z) - 12*y - 6*z + 12 + c <= k) {
+        n = n + 1;
+        x = x + y;
+        y = y + z;
+        z = z + 6;
+        c = c + 1;
+    }
+    p=1;
+    /* while(1) { if(dummy>0) break; } L_return: return 0; */
+
+    return 0;
+}
