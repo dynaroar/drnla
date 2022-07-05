c0816d5fa61979c5cd965707aa3a3cde11a6584f
Cyrus <cyrus.cl@outlook.com>
2022-07-05 14:48:29 -0400

    ctl injects.

--- benchmarks/ctl-multi/square-loop-nla-valid.c
+++ benchmarks/ctl-multi/square-loop-nla-valid.c
@@ -3,9 +3,12 @@ int main() {
     int x, y;
     x = __VERIFIER_nondet_int();
     y = __VERIFIER_nondet_int();
+    int p = 2;
     while (y>0){
         if (x*x>49) y = y-5;
         y--;
+        p = 1;
     }
+    p = 0;
     return x;
 }
--- benchmarks/ctl-multi/while-cubic-valid.c
+++ benchmarks/ctl-multi/while-cubic-valid.c
@@ -2,12 +2,11 @@ extern int __VERIFIER_nondet_int(void);
 int main() {
     int y;
     y = __VERIFIER_nondet_int();
-    /* int p = 2; */
+    int p = 2;
     while(y*y*y<=64 && y*y >= 1){
          y++;
-         /* p = 1; */
+         p = 1;
     }
-    /* p = 0; */
-    /* p = 0; */
+    p = 0;
     return 0;
 }
