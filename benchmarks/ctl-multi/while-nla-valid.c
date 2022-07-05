b9d1a2675fc22a24a9a81ffc30fdb2519965a87f
Cyrus <cyrus.cl@outlook.com>
2022-07-05 15:26:15 -0400

    while-nla ctl.

--- benchmarks/ctl-multi/while-nla-valid.c
+++ benchmarks/ctl-multi/while-nla-valid.c
@@ -3,13 +3,13 @@ int main() {
     int x, y;
     x = __VERIFIER_nondet_int();
     y = __VERIFIER_nondet_int();
-
+    int p = 2;
     while(y>0){
 
         if (x*x - 2*x >= 63) y = y-2;
-
         y--;
+        p = 1;
     }
-    
+    p = 0;
     return 0;
 }
